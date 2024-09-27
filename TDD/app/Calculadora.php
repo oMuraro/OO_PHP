3b1e25e67e66cb3ae99cb09bcbcbb9ce738b5dab
oMuraro <pedrooliveiramuraro@gmail.com>
2024-09-27 13:58:54 -0300

    asserts tests

--- TDD/app/Calculadora.php
+++ TDD/app/Calculadora.php
@@ -10,5 +10,17 @@
         public function soma($a, $b){
             return $a + $b;
         }
+
+        public function multiplicacao($a, $b){
+            return $a * $b;
+        }
+
+        public function divisao($a, $b){
+            return $a / $b;
+        }
+
+        public function subtracao($a, $b){
+            return $a - $b;
+        }
     }
 ?>
--- TDD/tests/CalculadoraTest.php
+++ TDD/tests/CalculadoraTest.php
@@ -2,11 +2,33 @@
 namespace tests;
 use PHPUnit\Framework\TestCase;
 use app\Calculadora;
+
 class CalculadoraTest extends TestCase{
     public function test_soma(){
         $calc = new Calculadora();
         $this->assertInstanceOf(Calculadora::class, $calc);
         $resultado = $calc->soma(2,3);
+        $this->assertEquals(5, $resultado);
+    }
+
+    public function test_multiplicacao(){
+        $calc = new Calculadora();
+        $this->assertInstanceOf(Calculadora::class, $calc);
+        $resultado = $calc->multiplicacao(10,10);
+        $this->assertEquals(100, $resultado);
+    }
+
+    public function test_divisao(){
+        $calc = new Calculadora();
+        $this->assertInstanceOf(Calculadora::class, $calc);
+        $resultado = $calc->divisao(12,2);
+        $this->assertEquals(6, $resultado);
+    }
+
+    public function test_subtracao(){
+        $calc = new Calculadora();
+        $this->assertInstanceOf(Calculadora::class, $calc);
+        $resultado = $calc->subtracao(8,3);
         $this->assertEquals(5, $resultado);
     }
 
